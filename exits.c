0b3905bd1857501432c139f2861acd47ddc1c3a5
Lordzhin1 <isaacaniapa32gmail.com>
2023-05-24 14:30:28 -0700

    simple shell pudies

---
+++ exits.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **_strncpy - duplicates a string
+ *@dest: the location string to be copied to
+ *@src: the origin string
+ *@n: the amount of char to be copied
+ *Return: the linked string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - link two strings
+ *@dest: link the first string
+ *@src: the second string
+ *@n: the amount of bytes to be fully used
+ *Return: the linked string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - identify a char in a string
+ *@s: the string to be pressed on
+ *@c: the char to find
+ *Return: (s) a pointer to around memory s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
